37df2306aced8c0eb74f04d98af03510a2efea51
Rafi Dudekula <rafi.dudekula@gmail.com>
2017-01-13 17:13:24 -0600

    added styles

--- src/App.js
+++ src/App.js
@@ -33,17 +33,17 @@ const Search = ({ value, onChange, children }) =>
   /> </form>
 
 const Table = ({list, searchTerm, onDismiss}) =>
-  <div>
+  <div className="table">
       {list.filter(isSearched(searchTerm)).map( item => 
-        <div key={item.objectID}> 
-          <span>
+        <div key={item.objectID} className="table-row"> 
+          <span style={{ width: '40%' }}>
             <a href={item.url}>{item.title}</a>
           </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-            <button onClick={() => onDismiss(item.objectID)} type="button">
+          <span style={{ width: '30%' }}>{item.author}</span>
+          <span style={{ width: '10%' }}>{item.num_comments}</span>
+          <span style={{ width: '10%' }}>{item.points}</span>
+          <span style={{ width: '10%' }}>
+            <button onClick={() => onDismiss(item.objectID)} type="button" className="button-inline">
               Dismiss
             </button>
           </span>
@@ -74,11 +74,13 @@ class App extends Component {
     const {list, searchTerm} = this.state
 
     return (
-      <div className="App">
-        <Search
-            value={searchTerm}
-            onChange={this.onSearchChange}>
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search
+              value={searchTerm}
+              onChange={this.onSearchChange}>
+          </Search>
+        </div>
         <Table 
           list={list} 
           searchTerm={searchTerm}
